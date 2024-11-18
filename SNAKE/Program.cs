d8766a393aae6a626a715cdd857b0a56614d63fa
Максим Слоква <slokvami@mail.ru>
2024-11-18 16:34:47 +0300

    Урок 2. Процедурный подход

--- SNAKE/Program.cs
+++ SNAKE/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x1 = 3;
+            int y1 = 3;
+            char sym1 = '#';
+
+            Draw(x1, y1, sym1);
+
+            int x2 = 5;
+            int y2 = 6;
+            char sym2 = '$';
+
+            Draw(x2, y2, sym2);
+
+            static void Draw(int x, int y, char sym)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(sym);
+            }
         }
     }
 }
