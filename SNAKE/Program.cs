9d5e2e1d8ad186f5e05bcbe51870ec11331c1026
Максим Слоква <slokvami@mail.ru>
2024-11-18 17:26:06 +0300

    Урок 6. Осваиваем класс «Список» Создал список и добавил туда наши точки

--- SNAKE/Program.cs
+++ SNAKE/Program.cs
@@ -10,6 +10,34 @@
             Point p2 = new Point(5, 6, '$');
             p2.Draw();
 
+            Point p3 = new Point(2, 2, '*');
+            p3.Draw();
+
+            Point p4 = new Point(4, 1, '@');
+            p4.Draw();
+
+            Console.WriteLine();
+
+            List<char> list = new List<char>();
+            list.Add('#');
+            list.Add('$');
+            list.Add('*');            
+
+            foreach (char c in list) 
+            {
+                Console.WriteLine(c);
+            }
+
+            List<Point> list2 = new List<Point>();
+
+            list2.Add(p1);
+            list2.Add(p2);
+            list2.Add(p3);
+            list2.Add(p4);
+
+            
+
+
         }
     }
 }
