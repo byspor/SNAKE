9781c4c938828aa5125e9308ac4f54473ac5ff94
Максим Слоква <slokvami@mail.ru>
2024-11-18 16:51:52 +0300

    Урок 3. Классы и объекты создал класс Point и перенес туда перменные и Draw

--- SNAKE/Program.cs
+++ SNAKE/Program.cs
@@ -4,23 +4,26 @@
     {
         static void Main(string[] args)
         {
-            int x1 = 3;
-            int y1 = 3;
-            char sym1 = '#';
+            Point p1 = new Point();
 
-            Draw(x1, y1, sym1);
+            p1.x = 3;
+            p1.y = 3;
+            p1.sym = '#';
+            p1.Draw();
 
-            int x2 = 5;
-            int y2 = 6;
-            char sym2 = '$';
+            Point p2 = new Point();
 
-            Draw(x2, y2, sym2);
+            p2.x = 5;
+            p2.y = 6;
+            p2.sym = '$';
+            p2.Draw();
+            
+            
+            
 
-            static void Draw(int x, int y, char sym)
-            {
-                Console.SetCursorPosition(x, y);
-                Console.Write(sym);
-            }
+            
+
+            
         }
     }
 }
